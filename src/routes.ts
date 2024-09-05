91b672232520dd220b9a6ed683d06ce613f3c6a3
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-09-04 23:17:17 -0300

    criando requisições

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUseController";
 
 const router = Router();
 router.get("/test", (request: Request, response: Response) => {
@@ -11,5 +12,6 @@ router.get("/test", (request: Request, response: Response) => {
 caira no metodo handle que pegara as informações passadas na requisição acionando o serviço
 CreateUserService que vai inserir os dados no banco de dados*/
 router.post("/user", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle) 
 
 export { router };
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,57 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import prismaClient from "../../prisma";
+import { AuthRequest } from "../../models/interfaces/auth/AuthRequest";
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    // verificando se o usuario digitou o email
+    if (!email) {
+      throw new Error("Digite seu email!");
+    }
+    // verificando se o usuario digitou o password
+    if (!password) {
+      throw new Error("Digite seu email!");
+    }
+
+    //verificar no banco de dados se existe um usuario com email passado
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    // caso não tenha email cadastrado ou usuario digitou email ou senha errados
+    if (!user) {
+      throw new Error("Usuario ou senha errado, tente novamente!");
+    }
+    // verificando se a senha está correta
+    const passwordWatch = await compare(password, user?.password);
+
+    if (!passwordWatch) {
+      throw new Error("Senha errada!");
+    }
+
+    // verificando tokem de criptografia da senha
+    const token = sign(
+      {
+        name: user?.name,
+        email: user?.email,
+      },
+      process.env.JWT_SECRETE as string, //passando o desincriptador do arquivo .env
+      {
+        subject: user?.id,
+        expiresIn: "30d", //apos 30 dias terar que fazer login novamente.
+      }
+    );
+
+    return {
+        id: user?.id,
+        name: user?.name,
+        email: user?.email,
+        token: token
+    }
+  }
+}
+
+export { AuthUserService };
