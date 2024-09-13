e6d0be2d171263bbea7c01392089cb373d133660
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-09-12 21:09:29 -0300

    Criando a documentação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes"; // importando o arquivo de rotas
+import cors from "cors" // para se comunicar com front end
 import "express-async-errors";
+import swaggerUI from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
+import path from "path"
+
 
 const app = express();
 const port = 3333;
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("v1",router);
+app.use("/api-doscs", swaggerUI.serve, swaggerUI.setup(swaggerDocument)); 
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")))
 
 // configurando as respostas de erro do "express-async-errors"
 app.use(
@@ -22,6 +30,12 @@ app.use(
     });
   }
 );
+
+app.get("/terms", (request: Request, response: Response) => {
+  return response.json({
+    message: "Termos de serviço"
+  })
+})
 
 app.listen(port, () => {
   console.log("Servidor rodando na porta 3333, Projeto Controle de Estoque!");
